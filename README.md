1728adae6776db1eadc2c82cf654c0f327d29e43
ktg5 <42522603+ktg5@users.noreply.github.com>
2024-01-17 03:29:41 -0800

    v1.3

--- README.md
+++ README.md
@@ -1,24 +1,21 @@
-# oocnichijou-bot
-The Twitter bot used for "oocnichijou".
-
-While this bot was bulit for just Nichijou, it could also be used for other shows too--not just Nichijou.
+# twitter-mediatoclip-bot
+A Twitter bot used for making clips out of randomly selected media.
 
 ## Requirements!!!
 * [npm & Node.JS](https://nodejs.org/en) (It's what the bot runs on, duh)
 * [ffmpeg](https://ffmpeg.org/download.html) (For encoding/making clips)
-* Legally obtained video files of Nichijou (Could be other anime too).
-  * Note: There can be **ANY** amount of videos in the `media` folder
-* [Access to the Twitter API (aka having a Twitter Application](https://developer.twitter.com) (Free or not)
-* A brain or two. (Just read the instructions below, one by one)
+* A collection of media for the bot to use
+* [Access to the Twitter API (aka having a Twitter Application for your account)](https://developer.twitter.com) (Free or not)
+* A brain or two (Just read the instructions below, one by one)
 
 ## How to work.
-1. Clone the repo - `git clone https://github.com/ktg5/oocnichijou-bot`
-2. Legally optain a copy of Nichijou in file format and place all episodes into the `media` folder, then delete the `_EPISODES GO HERE` file as it isn't needed.
-* Note: There can be **ANY** amount of videos in the `media` folder, as the bot reads all files inside the folder.
-  * BUT: This also means any file that isn't a video file inside the `media` folder will also be read by the bot, so **DON'T ADD ANYTHING INTO THE `media` FOLDER THAT ISN'T A VIDEO!!!**
+1. Clone the repo - `git clone https://github.com/ktg5/twitter-mediatoclip-bot`
+2. Place the media you'd like the bot to use into the `media` folder, then delete the `_EPISODES GO HERE`, file as it isn't needed.
+* Note: There can be **ANY** amount of videos in the `media` folder, as the bot reads all files inside the folder and selects a random one to be cliped.
+  * BUT: This also means any file that isn't a video will also be read by the bot, so **DON'T ADD ANYTHING INTO THE `media` FOLDER THAT ISN'T A VIDEO!!!**
 3. Install required packages. - `npm i`
 4. Open `auth.json` and replace all the listed keys (api & access) with your own, also make sure to set the Twitter username and video file format in `config.json`.
-* ⚠⚠ Remember to **make sure your access keys are "`Created with Read, and Write permissions`" by enabling "`User authentication`" in your Twitter app's page!**
+* ⚠⚠ Remember to **make sure your access keys are "`Created with Read, and Write permissions`" by enabling "`User authentication`" in your Twitter app's page and remaking the access token & secret!**
 5. Start the bot. - `npm start`
 
 If any errors occur, please report them to the issues page! Thank you!
--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const {client, logger} = require("./botAuth");
 const ffmpeg = require('fluent-ffmpeg');
 const path = require('node:path');
-const fs = require('fs');
+const fs = require('fs-extra');
+const editJsonFile = require('edit-json-file');
 const schedule = require('node-schedule');
 const config = require('./config.json');
 
@@ -10,32 +11,53 @@ function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+// get local db
+const db = editJsonFile(`${__dirname}/db.json`, {
+    autosave: true
+});
+
 console.log(`ｰｰｰｰｰｰｰｰｰｰ✄ｰｰｰｰｰｰｰｰｰｰ`);
 logger.info(`Starting process when it hits a new hour...`)
 
 // Start function
 async function startProcess() {
     console.log(`ｰｰｰｰｰｰｰｰｰｰ✄ｰｰｰｰｰｰｰｰｰｰ`);
-    // Get random epiosde.
-    /// Grab all the soggy files from the media directory // LOL THIS WAS FROM THE SOGGY CAT BOT
+    // Get random media.
     const videosPath = path.join(__dirname, 'media');
-    const imageFileNames = fs.readdirSync(videosPath)
+    const mediaFiles = fs.readdirSync(videosPath)
 
-    var epSelect = getRandomArbitrary(1, imageFileNames.length - 1);
-    
+    var selectedMedia = getRandomMedia(mediaFiles);
+    // Actual function so that we can call it back if it's the same as before
+    function getRandomMedia(mediaFiles) {
+        var selectedIndex = getRandomArbitrary(1, mediaFiles.length - 1);
+        var result = mediaFiles[selectedIndex];
+        // Make sure this one isn't the same as before
+        if (db.get('lastMedia')) {
+            if (db.get('lastMedia') !== result) {
+                // complete -- not a copy
+                db.set('lastMedia', result)
+                return result;
+            } else {
+                // do it again
+                getRandomMedia(mediaFiles);
+            }
+        }
+    }
+    var mediaPath = `./media/${selectedMedia}`;
+        
     // Get video information.
-    ffmpeg.ffprobe(`./media/${epSelect}.${config.videoFormat}`, async function(err, metadata) {
-        // Get episode's length.
+    ffmpeg.ffprobe(mediaPath, async function(err, metadata) {
+        // Get media's length.
         var videoLength = metadata.format.duration;
         // Get when the clip will start.
         var clipStart = getRandomArbitrary(0, videoLength - config.clipTime);
         // If you're wondering what the filename is,
         // it's just the time the clip was created
         // in UNIX time.
-        var filepath = `clips/${Math.floor(new Date().getTime() / 1000)}.mp4`
+        var filepath = `./clips/${Math.floor(new Date().getTime() / 1000)}.mp4`
 
         // Now we'll be cooking up a delicious "clip"...
-        ffmpeg({source: `./media/${epSelect}.${config.videoFormat}`})
+        ffmpeg({source: mediaPath})
         .setStartTime(clipStart) // When the clip will start.
         .duration(config.clipTime) // How long the clip will go for.
         .on(`start`, function (cmd) { // When render is starting
@@ -62,7 +84,6 @@ async function startProcess() {
 
             // Now we're gonna try to upload the media & tweets.
             try {
-                
                 // Upload media first...
                 var mediaId = await client.v1.uploadMedia(filePath);
                 logger.info('Upload Completed');
@@ -79,12 +100,16 @@ async function startProcess() {
                 // If seconds is less than 10, add a "0" at the start.
                 if (seconds < 10) seconds = "0" + seconds;
 
-                var replyTweet = await client.v2.reply(`Episode ${epSelect}\nStarts at ${minutes}:${seconds}`, mainTweet.data.id );
+                // Let's also get that extension text out of the media name for this second post.
+                var fileExtension = path.extname(mediaPath);
+                var fileName = path.basename(mediaPath, fileExtension);
+
+                var replyTweet = await client.v2.reply(`Video: "${fileName}"\nStarts at ${minutes}:${seconds}`, mainTweet.data.id );
                 logger.info(`Sent reply tweet; https://twitter.com/${config.username}/status/${replyTweet.data.id}`);
             
-            } catch (error) {
+            } catch (err) {
                 logger.error(`Something happened while uploading / trying to tweet!!`);
-                logger.error(error);
+                logger.error(err);
             }
         }
     });
